3c28badfe9d03f02098c71a3b8f359baea8c7741
Renan Fagundes <renan@gmail.com>
2022-07-07 17:51:26 -0300

    implementando funções do api.js requisito 01

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,24 @@
 export async function getCategories() {
-  // Implemente aqui
+  const url = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const response = await fetch(url);
+  const requestJson = await response.json();
+  return requestJson;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(id, categoria) {
+  const urlId = 'https://api.mercadolibre.com/sites/MLB/search?category=$CATEGORY_ID';
+  const urlQuery = 'https://api.mercadolibre.com/sites/MLB/search?q=$QUERY';
+
+  const responseId = await fetch(urlId);
+  const responseQuery = await fetch(urlQuery);
+
+  const jsonId = responseId.json();
+  const jsonQuery = responseQuery.json();
+
+  if (id) {
+    return jsonId;
+  }
+  if (categoria) {
+    return jsonQuery;
+  }
 }
