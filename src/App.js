897c6e77040ba54b3644052de9ba82b58d68b5fd
Matheus Queiroz <matheus_giga@hotmail.com>
2022-07-07 16:53:06 -0300

    Teste 123

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React do grupo 5 - Pamela 2.0, Renan 2.0,
+          Learn React do grupo 5 - Pamela 2.0, Renan 2.0, Matheus
         </a>
       </header>
     </div>
