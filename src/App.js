3d1265264dd6b35f746babe91e9005851cdc9e29
Renan Fagundes <renan@gmail.com>
2022-07-07 16:11:17 -0300

    iniciando Proj grupo 5

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React do grupo 5
         </a>
       </header>
     </div>
