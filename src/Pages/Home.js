248ff80badc27de82be0d78ff136f8ef2ac01c1d
Joao Moreira <jgmoreira1809@gmail.com>
2022-07-07 15:14:21 -0700

    Requisito 3

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import Button from '../Components/Button';
 
 class Home extends React.Component {
   render() {
     return (
       <div>
-        <form>
-          <input type="text" />
-        </form>
+        <header>
+          <div>
+            <form>
+              <input type="text" />
+            </form>
+          </div>
+          <Button />
+        </header>
         <h4 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h4>
