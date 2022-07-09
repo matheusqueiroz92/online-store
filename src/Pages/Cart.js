58a3c497555c331d5e6b7301974e9130da2fb8fb
pamelamonteiro <pamelamonteiro_@hotmail.com>
2022-07-08 21:37:30 -0300

    [Grupo 5] Req 8

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import { getProductById } from '../services/api';
 
 class Cart extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      productsIds: [],
+    };
+  }
+
+  componentDidMount() {
+    this.handleCartItems();
+  }
+
+  handleCartItems = async () => {
+    const { cartItems } = this.props;
+    console.log(cartItems);
+    const productsList = await getProductById(cartItems);
+    const { title, price, available_quantity } = productsList;
+    console.log(title, price, available_quantity);
+    const fullProducts = productsList;
+    this.setState((prevState) => ({
+      productsIds: [...prevState.productsIds, fullProducts],
+    }));
+  }
+
   render() {
+    const { productsIds } = this.state;
+    console.log(productsIds);
     return (
       <div>
+        {/* {
+          cartItems.length > 0
+            ? (
+              cartItems.map(() => )
+            )
+        } */}
         <h2 data-testid="shopping-cart-empty-message">
           Seu carrinho está vazio
         </h2>
